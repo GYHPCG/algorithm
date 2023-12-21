e817657d49ed4a597281cb76664b464ae8b04bd3
cgp <2986779260@qq.com>
2023-12-21 19:12:55 +0800

    2023

--- LanQiao/ACWing_base/test_con1.c
+++ LanQiao/ACWing_base/test_con1.c
@@ -1,19 +1,7 @@
 #include <stdio.h>
-
-void minmax(int a[], int len, int* p1, int* p2)
-{
-	*p1 = *p2 = a[0];
-	for (int i = 0; i < len - 1; i++)
-	{
-		a[i] > a[i + 1]? (*p2 = a[i + 1]): (*p1 = a[i + 1]);
-	}
-}
 int main()
 {
-	int a[5] = { 6, 3,4, 5, 1 };
-	int len = 5;
-	int ma, mi;
-	minmax(a, sizeof(a) / sizeof(0), &ma, &mi);
-	printf("%d %d\n", ma, mi);
+	int a[5];
+    a+1 = "\0";
 	return 0;
 }
--- competition/2023baidu/7.cpp
+++ competition/2023baidu/7.cpp
@@ -3,16 +3,21 @@
  * @Author: chenggong Pan
  * @Date: 2023-08-13 13:37:09
  * @LastEditors: chenggong Pan
- * @LastEditTime: 2023-08-13 13:37:15
+ * @LastEditTime: 2023-10-23 19:36:18
  */
-#include<iostream>
-#include <cstring>
-#include <algorithm>
-using namespace std;
-int main() {
-    // cout << "helllo, world" << endl;
-    int a, b;
-    scanf("%d,%d", &a, &b);
-    cout << a + b << endl;
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    float sum = 100, high = 50;
+    scanf("%d", &n);
+    for (i = 1; i < n; i++)
+    {
+        sum = sum + 2 * high;
+        high = high / 2;
+    }
+    printf("%.4f %.4f", sum, high);
+
     return 0;
 }
